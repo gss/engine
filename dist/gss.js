b9debf5736e9bba6610a9c79f2d09e06bb79dfc7
Yaroslaff Fedin <invizko@gmail.com>
2014-12-23 13:27:21 +0800

    compile gss

--- dist/gss.js
+++ dist/gss.js
@@ -26244,7 +26244,7 @@ Stylesheet = (function(_super) {
       for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
         rule = _ref2[_i];
         text = rule.cssText.replace(/\[matches~="(.*?)"\]/g, function(m, selector) {
-          return selector.replace(/@\d+/g, '').replace(/↓/g, ' ');
+          return selector.replace(/@[^↓]+/g, '').replace(/↓&/g, '').replace(/↓/g, ' ');
         });
         sheet.push(text);
       }
@@ -28744,23 +28744,33 @@ module.exports = Console;
 
 });
 require.register("gss/lib/utilities/Exporter.js", function(exports, require, module){
-var Exporter;
+var Exporter,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
 Exporter = (function() {
-  function Exporter() {}
+  function Exporter(engine) {
+    var _ref;
+    this.engine = engine;
+    this.postexport = __bind(this.postexport, this);
+    this.preexport = __bind(this.preexport, this);
+    if (!(this.command = (_ref = location.search.match(/export=([a-z0-9]+)/)) != null ? _ref[1] : void 0)) {
+      return;
+    }
+    engine.addEventListener('precompile', this.preexport);
+  }
 
   Exporter.prototype.preexport = function() {
-    var baseline, element, height, match, pairs, scope, width, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref, _ref1, _ref2, _ref3, _ref4, _ref5, _ref6,
+    var baseline, element, height, pairs, scope, width, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref, _ref1, _ref2, _ref3, _ref4, _ref5,
       _this = this;
-    if ((scope = this.scope).nodeType === 9) {
-      scope = this.scope.body;
+    if ((scope = this.engine.scope).nodeType === 9) {
+      scope = this.engine.scope.body;
     }
-    this.identify(scope);
+    this.engine.identify(scope);
     _ref = scope.getElementsByTagName('*');
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       element = _ref[_i];
       if (element.tagName !== 'SCRIPT' && (element.tagName !== 'STYLE' || ((_ref1 = element.getAttribute('type')) != null ? _ref1.indexOf('gss') : void 0) > -1)) {
-        this.identify(element);
+        this.engine.identify(element);
       }
     }
     if (window.Sizes) {
@@ -28779,28 +28789,26 @@ Exporter = (function() {
         }
       }
     }
-    if (match = (_ref5 = location.search.match(/export=([a-z0-9]+)/)) != null ? _ref5[1] : void 0) {
-      if (match.indexOf('x') > -1) {
-        _ref6 = match.split('x'), width = _ref6[0], height = _ref6[1];
-        baseline = 72;
-        width = parseInt(width) * baseline;
-        height = parseInt(height) * baseline;
-        window.addEventListener('load', function() {
-          localStorage[match] = JSON.stringify(_this["export"]());
-          return _this.postexport();
-        });
-        document.body.style.width = width + 'px';
-        this.intrinsic.properties['::window[height]'] = function() {
-          return height;
-        };
-        return this.intrinsic.properties['::window[width]'] = function() {
-          return width;
-        };
-      } else {
-        if (match === 'true') {
-          localStorage.clear();
-          return this.postexport();
-        }
+    if (this.command.indexOf('x') > -1) {
+      _ref5 = this.command.split('x'), width = _ref5[0], height = _ref5[1];
+      baseline = 72;
+      width = parseInt(width) * baseline;
+      height = parseInt(height) * baseline;
+      window.addEventListener('load', function() {
+        localStorage[_this.command] = JSON.stringify(_this["export"]());
+        return _this.postexport();
+      });
+      document.body.style.width = width + 'px';
+      this.engine.intrinsic.properties['::window[height]'] = function() {
+        return height;
+      };
+      return this.engine.intrinsic.properties['::window[width]'] = function() {
+        return width;
+      };
+    } else {
+      if (this.command === 'true') {
+        localStorage.clear();
+        return this.postexport();
       }
     }
   };
@@ -28828,20 +28836,20 @@ Exporter = (function() {
   Exporter.prototype["export"] = function() {
     var id, index, path, property, value, values, _ref;
     values = {};
-    _ref = this.values;
+    _ref = this.engine.values;
     for (path in _ref) {
       value = _ref[path];
       if ((index = path.indexOf('[')) > -1 && path.indexOf('"') === -1) {
-        property = this.camelize(path.substring(index + 1, path.length - 1));
+        property = this.engine.intrinsic.camelize(path.substring(index + 1, path.length - 1));
         id = path.substring(0, index);
         if (property === 'x' || property === 'y' || document.body.style[property] !== void 0) {
-          if (this.values[id + '[intrinsic-' + property + ']'] == null) {
+          if (this.engine.values[id + '[intrinsic-' + property + ']'] == null) {
             values[path] = Math.ceil(value);
           }
         }
       }
     }
-    values.stylesheets = this.stylesheets["export"]();
+    values.stylesheets = this.engine.Stylesheet["export"]();
     return values;
   };
 
@@ -28849,7 +28857,7 @@ Exporter = (function() {
 
 })();
 
-module["export"] = Exporter;
+module.exports = Exporter;
 
 });
 require.register("gss/lib/utilities/Inspector.js", function(exports, require, module){
